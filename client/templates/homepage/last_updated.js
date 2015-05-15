c467ad3f9d4b8b557cca16f491a6b1a930110163
scattm <ntngh2712@gmail.com>
2015-05-16 00:30:51 +0700

    Re-Format last update date hh:MM:ss dd/mm/yyyy

--- client/templates/homepage/last_updated.js
+++ client/templates/homepage/last_updated.js
@@ -2,7 +2,7 @@ Template.lastUpdated.helpers({
 	getLastUpdated: function(){
 		oneRow = MatchingScores.findOne({}, {sort: {updateDate: -1}});
 		lastUpdated = new Date(oneRow.updateDate);
-		retString = '<em>Cập nhật gần nhất vào lúc <strong>' + lastUpdated.getHours() + ':' + lastUpdated.getMinutes() + ':' + lastUpdated.getSeconds() +
+		retString = '<em>Cập nhật gần nhất vào lúc <strong>' + ("0" + lastUpdated.getHours()).slice(-2) + ':' + ("0" + lastUpdated.getMinutes()).slice(-2) + ':' + ("0" + lastUpdated.getSeconds()).slice(-2) +
 			' ' + ("0" + lastUpdated.getDate()).slice(-2) + '/' + ("0" + (lastUpdated.getMonth() + 1)).slice(-2) + '/' + lastUpdated.getFullYear() + '</strong>.</em>';
 		return retString;
 	}
