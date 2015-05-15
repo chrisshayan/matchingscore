8937a11a3e2189c97def0360678a64bf93e157d1
nghiant <Nghia.Nguyen@navigosgroup.com>
2015-05-15 17:54:41 +0700

    Update date format for last update.

--- client/templates/homepage/last_updated.js
+++ client/templates/homepage/last_updated.js
@@ -1,8 +1,9 @@
 Template.lastUpdated.helpers({
 	getLastUpdated: function(){
-		lastUpdated = new Date(MatchingScores.findOne({}, {sort: {updateDate: -1}}).updateDate);
+		oneRow = MatchingScores.findOne({}, {sort: {updateDate: -1}});
+		lastUpdated = new Date(oneRow.updateDate);
 		retString = '<em>Cập nhật gần nhất vào lúc <strong>' + lastUpdated.getHours() + ':' + lastUpdated.getMinutes() + ':' + lastUpdated.getSeconds() +
-			' ' + lastUpdated.getDay() + '/' + lastUpdated.getMonth() + '/' + lastUpdated.getYear() + '</strong>.</em>';
+			' ' + ("0" + lastUpdated.getDate()).slice(-2) + '/' + ("0" + (lastUpdated.getMonth() + 1)).slice(-2) + '/' + lastUpdated.getFullYear() + '</strong>.</em>';
 		return retString;
 	}
 });
