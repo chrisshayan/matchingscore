44a8c985c28fc9cb039996750dac811e615f1e1d
nghiant <Nghia.Nguyen@navigosgroup.com>
2015-05-15 18:53:04 +0700

    Simple paging matching score list

--- client/templates/homepage/matching_scores_list.js
+++ client/templates/homepage/matching_scores_list.js
@@ -2,11 +2,13 @@ Template.matchingScoresList.onCreated(function () {
 	var instance = this;
 
 	instance.selectedLocation = new ReactiveVar(-1);
+	instance.loaded = new ReactiveVar(5);
+	instance.limit = 5;
 
 	instance.autorun(function (){
 		var selectedLocation = instance.selectedLocation.get();		
 		var subscription = instance.subscribe('matchingscores', selectedLocation);
-		//console.log('subscribe matchingscores:' + selectedLocation);
+		
 		if (subscription.ready()){
 			console.log("> Received matchingscore for cityId " + selectedLocation + "\n\n");
 		} else {
@@ -22,15 +24,28 @@ Template.matchingScoresList.helpers({
     		selectedLocation = -1
     	}
 
-    	return MatchingScores.find({cityId: selectedLocation}, { sort: { avgMatchingScore: -1 } });
+    	return MatchingScores.find({cityId: selectedLocation}, { sort: { avgMatchingScore: -1 } , limit: Template.instance().loaded.get() });
+	},
+	hasMore: function (){
+		return MatchingScores.find({}).count() > Template.instance().loaded.get();
 	}
 });
 
 Template.matchingScoresList.events({
     "change #selectedLocation": function(event, instance){
+    	event.preventDefault();
+    	
         var selectedLocation = instance.selectedLocation.get();
         selectedLocation = $(event.target).find('option:selected').val();
         instance.selectedLocation.set(selectedLocation);
+        instance.loaded.set(instance.limit);
         Session.set('selectedLocation', selectedLocation);
+    },
+    "click .load-more": function(event, instance){
+    	event.preventDefault();
+
+    	var loaded = instance.loaded.get();
+    	loaded += instance.limit;
+    	instance.loaded.set(loaded);
     }
 });
