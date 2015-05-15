55cf0e63df28ea6add8961ba55a784bba87c56cb
Ly Duong <ly.duong@navigosgroup.com>
2015-05-15 16:58:21 +0700

    Update FB Share

--- client/templates/homepage/last_updated.js
+++ client/templates/homepage/last_updated.js
@@ -1,8 +1,8 @@
 Template.lastUpdated.helpers({
 	getLastUpdated: function(){
 		lastUpdated = new Date(MatchingScores.findOne({}, {sort: {updateDate: -1}}).updateDate);
-		retString = 'Cập nhật gần nhất vào lúc ' + lastUpdated.getHours() + ':' + lastUpdated.getMinutes() + ':' + lastUpdated.getSeconds() +
-			' ' + lastUpdated.getDay() + '/' + lastUpdated.getMonth() + '/' + lastUpdated.getYear() + '.';
+		retString = '<em>Cập nhật gần nhất vào lúc <strong>' + lastUpdated.getHours() + ':' + lastUpdated.getMinutes() + ':' + lastUpdated.getSeconds() +
+			' ' + lastUpdated.getDay() + '/' + lastUpdated.getMonth() + '/' + lastUpdated.getYear() + '</strong>.</em>';
 		return retString;
 	}
 });
