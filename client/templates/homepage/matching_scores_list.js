f1c375b919af0bd2046be7af4b5b1bbadf463919
nghiant <Nghia.Nguyen@navigosgroup.com>
2015-05-18 18:45:59 +0700

    Implement HighCharts on top 5 application industry

--- client/templates/homepage/matching_scores_list.js
+++ client/templates/homepage/matching_scores_list.js
@@ -1,7 +1,7 @@
 Template.matchingScoresList.onCreated(function () {
 	var instance = this;
 
-	instance.limit = 5;
+	instance.limit = 10;
 	instance.loadCondition = new ReactiveVar({
 		selectedLocation: -1,
 		loadAllowed: instance.limit + 1
@@ -27,7 +27,71 @@ Template.matchingScoresList.helpers({
     	return MatchingScores.find({ cityId: loadCondition.selectedLocation }, { sort: { avgMatchingScore: -1 } , limit: (loadCondition.loadAllowed - 1) });
 	},
 	hasMore: function (){
-		return MatchingScores.find({}).count() > Template.instance().loadCondition.get().loadAllowed - 1;
+		var loadCondition = Template.instance().loadCondition.get();
+
+		return MatchingScores.find({ cityId: loadCondition.selectedLocation }).count() > Template.instance().loadCondition.get().loadAllowed - 1;
+	},
+	drawChart: function() {
+		var loadCondition = Template.instance().loadCondition.get();
+
+		top5MatchingScore = MatchingScores.find({ cityId: loadCondition.selectedLocation }, {sort: {countMatchingScore: -1}, limit: 5}).fetch();
+
+		var matchingScores = {
+			industry: [],
+			min: [],
+			max: [],
+			avg: []
+		}
+	
+		for (i = 0; i < top5MatchingScore.length; i++) {
+			industry = 
+				MasterData.findOne({dataType: "category", categoryId: top5MatchingScore[i].industryId}).categoryVNName +
+				'(' + top5MatchingScore[i].countMatchingScore + ')';
+			matchingScores.industry.push(industry);
+			matchingScores.min.push(top5MatchingScore[i].minMatchingScore);
+			matchingScores.max.push(top5MatchingScore[i].maxMatchingScore);
+			matchingScores.avg.push(top5MatchingScore[i].avgMatchingScore);
+		}
+	
+		$('#top-five-chart').highcharts({
+			chart: {
+				type: 'column'
+			},
+			title: {
+				text: 'Biểu đồ Matching Score',
+				x: -20
+			},
+			subtitle: {
+				text: '5 ngành nghề được ứng tuyển nhiều nhất',
+				x: -20
+			},
+			xAxis: {
+				categories: matchingScores.industry
+			},
+			yAxis: {
+				title: {
+					text: 'Points'
+				},
+				min: 0,
+				max: 100
+			},
+			legend: {
+            	layout: 'vertical',
+            	align: 'right',
+            	verticalAlign: 'middle',
+            	borderWidth: 0
+        	},
+        	series: [{
+        		name: 'Min',
+        		data: matchingScores.min
+        	}, {
+        		name: 'Max',
+        		data: matchingScores.max
+        	}, {
+        		name: 'Average',
+        		data: matchingScores.avg
+        	}]
+		});
 	}
 });
 
