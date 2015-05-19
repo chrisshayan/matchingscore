9d8b1d4567316f332dc72ba5c79dd597016213c1
nghiant <Nghia.Nguyen@navigosgroup.com>
2015-05-19 16:45:38 +0700

    Show how long matching score based on

--- client/templates/homepage/last_updated.js
+++ client/templates/homepage/last_updated.js
@@ -3,7 +3,13 @@ Template.lastUpdated.helpers({
 		oneRow = MatchingScores.findOne({}, {sort: {updateDate: -1}});
 		lastUpdated = new Date(oneRow.updateDate);
 		retString = '<em>Cập nhật lần cuối vào lúc <strong>' + ("0" + lastUpdated.getHours()).slice(-2) + ':' + ("0" + lastUpdated.getMinutes()).slice(-2) + ':' + ("0" + lastUpdated.getSeconds()).slice(-2) +
-			' ' + ("0" + lastUpdated.getDate()).slice(-2) + '/' + ("0" + (lastUpdated.getMonth() + 1)).slice(-2) + '/' + lastUpdated.getFullYear() + '</strong>.</em>';
+			' ' + ("0" + lastUpdated.getDate()).slice(-2) + '/' + ("0" + (lastUpdated.getMonth() + 1)).slice(-2) + '/' + lastUpdated.getFullYear() + '</strong></em>';
+		return retString;
+	},
+	getCronPeriod: function(){
+		retString = '<em>dựa theo số lượt ứng tuyển trong vòng <strong>' + 
+			Meteor.settings.public.cronJobPeriod + 
+			'</strong></em>';
 		return retString;
 	}
 });
